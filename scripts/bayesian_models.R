7734ada9221e59d24bed6eaa49771acbe5806123
Bodo Winter <bodowinter@192.168.1.152>
2021-03-31 11:08:50 +0100

    updated armenian vs. amharic coding error

--- scripts/bayesian_models.R
+++ scripts/bayesian_models.R
@@ -9,8 +9,15 @@
 library(tidyverse)
 library(brms)
 
+## Load data:
+
 field <- read_csv('../data/field_cleaned.csv')
 web <- read_csv('../data/online_cleaned.csv')
+
+## Fix Armenian / Amharic issue (this miscoding was something we noted after submission of the first version):
+
+web <- mutate(web,
+              Genus = ifelse(Genus == 'Afro-Asiatic', 'IE', Genus))
 
 ## For parallel processing:
 
@@ -20,6 +27,20 @@ options(mc.cores=parallel::detectCores())
 
 my_priors <- prior('uniform(-10, 10)',
                    class = 'Intercept')
+my_priors_with_beta <- c(prior('uniform(-10, 10)',
+                             class = 'Intercept'),
+                         prior('normal(0, 2)',
+                               class = 'b'))
+
+## For the field model we fit animacy as it's balanced and they are all nouns:
+
+field <- mutate(field,
+                AnimacyCat = factor(Animacy, levels = c('inanimate', 'animate')))
+contrasts(field$AnimacyCat) <- contr.sum(2) * -1
+
+## Check that inanimate = -1:
+
+contrasts(field$AnimacyCat)
 
 ## Fit the main model:
 
@@ -38,7 +59,7 @@ main_mdl <- brm(ACC ~ 1 +
                 control = list(adapt_delta = 0.99,
                                max_treedepth = 13))
 save(main_mdl, file = 'main_mdl.RData',
-     compress = 'xz', compression_level = 9))
+     compress = 'xz', compression_level = 9)
 
 ## Fit the model without animacy:
 
@@ -57,5 +78,25 @@ field_mdl <- brm(ACC ~ 1 +
                                 max_treedepth = 13))
 save(field_mdl, file = 'field_mdl.Rdata',
      compress = 'xz', compression_level = 9)
+
+## Fit the model with animacy:
+
+field_animacy_mdl <- brm(ACC ~ 1 + Animacy +
+                   (1 + Animacy|ID) +
+                   (1|Meaning) + (1|UniqueItem) +
+                   (1|Team) +
+                   (1 + Animacy|Language),
+                 data = field,
+                 family = bernoulli,
+                 init = 0, seed = 42,
+                 cores = 4, chains = 4,
+                 warmup = 2000, iter = 4000,
+                 prior = my_priors_with_beta,
+                 control = list(adapt_delta = 0.99,
+                                max_treedepth = 13))
+save(field_animacy_mdl, file = 'field_animacy_mdl.Rdata',
+     compress = 'xz', compression_level = 9)
+
+
 
 
